659fc478b6eb156bf8f43280052dee118c9c3932
Nate Laffan <nate.laffan@gmail.com>
2024-07-13 02:02:47 -0700

    Housekeeping. Started introduction project

---
+++ demos/introduction/src/scenes/Load.js
@@ -0,0 +1,33 @@
+export class LoadScene extends Phaser.Scene {
+  constructor() {
+    super("LoadScene");
+  }
+
+  preload() {
+    // Start loading PSD data
+    this.P2P.load.load(this, "psd_key", "assets/introduction");
+
+    // Listen for PSD asset loading progress
+    this.events.on("psdLoadProgress", (value) => {
+      console.log("Loading progress:", value);
+    });
+
+    // Listen for PSD asset loading completion
+    this.events.once(
+      "psdLoadComplete",
+      () => {
+        console.log("PSD assets loading complete");
+        this.scene.start("PlayScene");
+
+        // What got loaded?
+        console.log("Loaded PSD data:", this.P2P.getData("psd_key"));
+      },
+      this
+    );
+
+    // Add error listener
+    this.load.on("loaderror", (fileObj) => {
+      console.error("Error loading file:", fileObj.key);
+    });
+  }
+}
