db7f49e5b8f87990ab7d94d17a111bd05f1ad3b4
Nate Laffan <nate.laffan@gmail.com>
2024-08-04 16:05:46 -0700

    Make sprite placement absolute, change placement to instances

--- generator/src/types/sprite_types/spritesheet.py
+++ generator/src/types/sprite_types/spritesheet.py
@@ -1,23 +1,28 @@
 import os
 import math
 from PIL import Image
+from psd_tools.api.layers import Layer, Group
 from src.types.sprite import Sprite
 from src.helpers.optimize_pngs import optimize_pngs
 from src.helpers.parsers import parse_attributes
 
 class SpritesheetSprite(Sprite):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.frame_dict = {}
+    def __init__(self, layer_info, layer, config, output_dir, psd_name):
+        super().__init__(layer_info, layer, config, output_dir, psd_name)
         self.frames = []
+        self.unique_frames = {}
+        self.instances = []
+        self.layer_order_counter = 0
         self.max_width = 0
         self.max_height = 0
-        self.layer_order_counter = 0
 
     def process(self):
         self._collect_frames()
-        spritesheet = self._create_spritesheet()
-        file_path = self._save_image(spritesheet)
+        if not self.unique_frames:
+            print(f"Warning: No valid frames found for spritesheet '{self.layer_info['name']}'. Skipping spritesheet creation.")
+            return None
+        spritesheet_image = self._create_spritesheet()
+        file_path = self._save_image(spritesheet_image)
 
         sprite_data = {
             **self.layer_info,
@@ -27,50 +32,48 @@ class SpritesheetSprite(Sprite):
             "frame_count": len(self.frames),
             "columns": self.columns,
             "rows": self.rows,
-            "placement": self._generate_placement()
+            "instances": self.instances,
+            "frames": self._generate_frames()
         }
 
         return sprite_data
 
     def _collect_frames(self):
-        for child in self.layer:
-            parsed_layer = parse_attributes(child.name)
-            if parsed_layer is None:
-                # If parsing fails, use the layer name as is
-                child_name = child.name
-                child_attributes = {}
-            else:
-                child_name = parsed_layer['name']
-                child_attributes = parsed_layer.get('attributes', {})
+        if isinstance(self.layer, Group):
+            for child in self.layer:
+                self._process_layer(child)
+        else:
+            print(f"Warning: Spritesheet layer '{self.layer_info['name']}' is not a group. Skipping spritesheet creation.")
+        self.frames = list(self.unique_frames.values())
 
-            child_image = child.composite()
+    def _process_layer(self, layer):
+        if layer is None or not layer.is_visible():
+            return
+        frame = self._create_frame(layer)
+        if frame:
+            if frame['name'] not in self.unique_frames:
+                self.unique_frames[frame['name']] = frame
+            self._add_instance(frame, layer)
 
-            self.max_width = max(self.max_width, child_image.width)
-            self.max_height = max(self.max_height, child_image.height)
+    def _create_frame(self, layer):
+        frame = {
+            'name': layer.name,
+            'image': layer.composite(),
+            'order': self.layer_order_counter,
+            'width': layer.width,
+            'height': layer.height
+        }
+        self.max_width = max(self.max_width, frame['width'])
+        self.max_height = max(self.max_height, frame['height'])
+        self.layer_order_counter += 1
+        return frame
 
-            frame_name = child_name
-            if frame_name not in self.frame_dict:
-                self.frame_dict[frame_name] = {
-                    'image': child_image,
-                    'index': len(self.frames),
-                    'instances': []
-                }
-                self.frames.append((frame_name, child_image))
-
-            instance_name = f"{frame_name}_{len(self.frame_dict[frame_name]['instances'])}"
-            instance_data = {
-                'attributes': child_attributes,
-                'left': child.left,
-                'top': child.top,
-                'layerOrder': self.layer_order_counter,
-                'instanceName': instance_name
-            }
-
-            self.frame_dict[frame_name]['instances'].append(instance_data)
-            self.layer_order_counter += 1
-
-        if not self.frames:
-            raise ValueError("No valid frames found in spritesheet")
+    def _add_instance(self, frame, layer):
+        self.instances.append({
+            'name': layer.name,
+            'x': layer.left,
+            'y': layer.top,
+        })
 
     def _create_spritesheet(self):
         frame_count = len(self.frames)
@@ -79,42 +82,34 @@ class SpritesheetSprite(Sprite):
 
         spritesheet = Image.new('RGBA', (self.columns * self.max_width, self.rows * self.max_height), (0, 0, 0, 0))
 
-        for index, (_, frame) in enumerate(self.frames):
+        for index, frame in enumerate(self.frames):
             x = (index % self.columns) * self.max_width
             y = (index // self.columns) * self.max_height
 
-            offset_x = (self.max_width - frame.width) // 2
-            offset_y = (self.max_height - frame.height) // 2
+            offset_x = (self.max_width - frame['width']) // 2
+            offset_y = (self.max_height - frame['height']) // 2
 
-            spritesheet.paste(frame, (x + offset_x, y + offset_y), frame)
+            spritesheet.paste(frame['image'], (x + offset_x, y + offset_y), frame['image'])
+            frame['spritesheet_x'] = x
+            frame['spritesheet_y'] = y
 
         return spritesheet
 
-    def _generate_placement(self):
-        placement = []
-
-        for name, frame_info in self.frame_dict.items():
-            frame_index = frame_info['index']
-
-            for instance in frame_info['instances']:
-                placement_entry = {
-                    "frame": frame_index,
-                    "x": instance['left'] - self.layer.left,
-                    "y": instance['top'] - self.layer.top,
-                    "layerOrder": instance['layerOrder'],
-                    "instanceName": instance['instanceName'],
-                    **instance['attributes']
-                }
-                placement.append(placement_entry)
-
-        return placement
+    def _generate_frames(self):
+        return {
+            frame['name']: {
+                'x': frame['spritesheet_x'],
+                'y': frame['spritesheet_y'],
+                'width': frame['width'],
+                'height': frame['height']
+            } for frame in self.frames
+        }
 
     def _save_image(self, image):
         filename = f"{self.layer_info['name']}.png"
         file_path = self.export_image(image, filename)
         self._optimize_image(file_path)
         return os.path.join('sprites', filename)  # Return the path relative to the PSD output directory
-
 
     def _optimize_image(self, file_path):
         optimize_config = self.config.get('optimizePNGs', {})
