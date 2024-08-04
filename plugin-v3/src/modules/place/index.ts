ab6d7e2e2d6105f9c8c1e28986f13ba70b26be41
Nate Laffan <nate.laffan@gmail.com>
2024-08-04 12:26:49 -0700

    attachMethod with place modules properly referenced

--- plugin-v3/src/modules/place/index.ts
+++ plugin-v3/src/modules/place/index.ts
@@ -1,7 +1,11 @@
 // src/modules/place/index.ts
 
 import PsdToPhaserPlugin from '../../PsdToPhaserPlugin';
-import { attachMethods, preserveOriginalMethods } from '../shared/attachMethods';
+import { placeTiles } from './types/tiles';
+import { placeSprites } from './types/sprites';
+import { placeZones } from './types/zones';
+import { placePoints } from './types/points';
+import { attachMethods } from '../shared/attachMethods';
 
 export default function placeModule(plugin: PsdToPhaserPlugin) {
   return function place(scene: Phaser.Scene, psdKey: string, layerPath: string, options: { depth?: number } = {}): Phaser.GameObjects.GameObject | Phaser.GameObjects.Group {
@@ -48,126 +52,37 @@ function placeLayerImmediately(
   options: { depth?: number }
 ): Phaser.GameObjects.GameObject | Phaser.GameObjects.Group {
   if (layer.category === 'group') {
-    const newGroup = scene.add.group();
-    preserveOriginalMethods(newGroup);
     if (Array.isArray(layer.children) && (options.depth === undefined || options.depth > 0)) {
       layer.children.forEach((child: any) => {
-        const childObject = placeLayerImmediately(scene, child, plugin, tileSliceSize, newGroup, psdKey, {
+        const childObject = placeLayerImmediately(scene, child, plugin, tileSliceSize, group, psdKey, {
           ...options,
           depth: options.depth !== undefined ? options.depth - 1 : undefined
         });
         if (childObject instanceof Phaser.GameObjects.GameObject) {
-          newGroup.add(childObject);
+          group.add(childObject);
         }
       });
+      return group;
     }
-    group.add(newGroup);
-    return newGroup;
+    return group;
   } else {
-    let placedObject: Phaser.GameObjects.GameObject | null = null;
+    let placedObject: Phaser.GameObjects.GameObject | Phaser.GameObjects.Group | null = null;
     switch (layer.category) {
       case 'tileset':
-        placedObject = placeTiles(scene, layer, plugin, tileSliceSize, group, psdKey);
+        placedObject = placeTiles(scene, layer, plugin, tileSliceSize, group, () => {}, psdKey);
         break;
       case 'sprite':
-        placedObject = placeSprites(scene, layer, plugin, group, psdKey);
+        placedObject = placeSprites(scene, layer, plugin, group, () => {}, psdKey);
         break;
       case 'zone':
-        placedObject = placeZones(scene, layer, plugin, group, psdKey);
+        placedObject = placeZones(scene, layer, plugin, group, () => {}, psdKey);
         break;
       case 'point':
-        placedObject = placePoints(scene, layer, plugin, group, psdKey);
+        placedObject = placePoints(scene, layer, plugin, group, () => {}, psdKey);
         break;
       default:
         console.error(`Unknown layer category: ${layer.category}`);
     }
     return placedObject || group;
   }
-}
-
-function placeTiles(scene: Phaser.Scene, layer: any, plugin: PsdToPhaserPlugin, tileSliceSize: number, group: Phaser.GameObjects.Group, psdKey: string): Phaser.GameObjects.GameObject {
-  const tilesGroup = scene.add.group();
-  
-  for (let col = 0; col < layer.columns; col++) {
-    for (let row = 0; row < layer.rows; row++) {
-      const x = layer.x + col * tileSliceSize;
-      const y = layer.y + row * tileSliceSize;
-      const key = `${layer.name}_tile_${col}_${row}`;
-
-      if (scene.textures.exists(key)) {
-        const tile = scene.add.image(x, y, key);
-        tile.setOrigin(0, 0);
-        tile.setDepth(layer.initialDepth || 0);
-        tilesGroup.add(tile);
-      }
-    }
-  }
-
-  group.add(tilesGroup);
-  return tilesGroup;
-}
-
-function placeSprites(scene: Phaser.Scene, layer: any, plugin: PsdToPhaserPlugin, group: Phaser.GameObjects.Group, psdKey: string): Phaser.GameObjects.GameObject {
-  let sprite: Phaser.GameObjects.Sprite;
-
-  if (layer.type === 'atlas') {
-    sprite = scene.add.sprite(layer.x, layer.y, layer.name, layer.frame);
-  } else if (layer.type === 'animation') {
-    sprite = scene.add.sprite(layer.x, layer.y, layer.name);
-    if (scene.anims.exists(layer.name)) {
-      sprite.play(layer.name);
-    }
-  } else {
-    sprite = scene.add.sprite(layer.x, layer.y, layer.name);
-  }
-
-  sprite.setOrigin(0, 0);
-  sprite.setDepth(layer.initialDepth || 0);
-
-  if (layer.alpha !== undefined) sprite.setAlpha(layer.alpha);
-  if (layer.visible !== undefined) sprite.setVisible(layer.visible);
-
-  group.add(sprite);
-  return sprite;
-}
-
-function placeZones(scene: Phaser.Scene, layer: any, plugin: PsdToPhaserPlugin, group: Phaser.GameObjects.Group, psdKey: string): Phaser.GameObjects.GameObject {
-  let zone: Phaser.GameObjects.Zone;
-
-  if (layer.subpaths && layer.subpaths.length > 0) {
-    const points = layer.subpaths[0].map((point: number[]) => new Phaser.Math.Vector2(point[0], point[1]));
-    const polygon = new Phaser.Geom.Polygon(points);
-    const bounds = polygon.getBounds();
-    zone = scene.add.zone(bounds.x, bounds.y, bounds.width, bounds.height);
-  } else {
-    zone = scene.add.zone(layer.x, layer.y, layer.width, layer.height);
-  }
-
-  zone.setOrigin(0, 0);
-  zone.setDepth(layer.initialDepth || 0);
-
-  Object.keys(layer).forEach((key) => {
-    if (!["name", "subpaths", "bbox", "children"].includes(key)) {
-      zone.setData(key, layer[key]);
-    }
-  });
-
-  group.add(zone);
-  return zone;
-}
-
-function placePoints(scene: Phaser.Scene, layer: any, plugin: PsdToPhaserPlugin, group: Phaser.GameObjects.Group, psdKey: string): Phaser.GameObjects.GameObject {
-  const point = new Phaser.GameObjects.GameObject(scene, 'point');
-  point.setData('x', layer.x);
-  point.setData('y', layer.y);
-  point.setData('name', layer.name);
-
-  Object.keys(layer).forEach((key) => {
-    if (!["name", "x", "y", "children"].includes(key)) {
-      point.setData(key, layer[key]);
-    }
-  });
-
-  group.add(point);
-  return point;
 }
