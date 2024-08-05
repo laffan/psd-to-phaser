04a55c212214b16c0ca1c8c881f696371b8e513f
Nate Laffan <nate.laffan@gmail.com>
2024-08-05 13:55:46 -0700

    Adding lazyLoad prep code to play.js

--- demos/introduction/src/scenes/Play.js
+++ demos/introduction/src/scenes/Play.js
@@ -8,6 +8,43 @@ export class PlayScene extends Scene {
   create() {
     console.log("Play scene reached");
 
+    this.P2P.placeAll(this, "psd_key")
+
+
+    this.lazyCam = this.P2P.createCamera(
+      this.cameras.main,
+      ["draggable", "lazyLoad"],
+      "simple_psd",
+      {
+        lazyLoad: {
+          extendPreloadBounds: -40,
+          debug: { shape: true },
+        },
+
+        draggable: {
+          // useBounds: { x: 0, y: 0, width: 1000, height: 1000 },
+          easeDragging: true,
+          friction: 0.95,
+          minSpeed: 0.1,
+        },
+      }
+    );
+
+this.events.on("lazyLoadStart", (progress, currentlyLoading) => {
+  console.log(`Loading is ${progress} complete.`);
+  console.log(currentlyLoading); // Array of items currently loading.
+});
+
+this.events.on("lazyLoadProgress", (progress, currentlyLoading) => {
+  console.log(`Loading is ${progress} complete.`);
+  console.log(currentlyLoading); // Array of items currently loading.
+});
+
+this.events.on("lazyLoadingComplete", () => {
+  console.log(`Lazy loading is complete.`);
+});
+
+
   }
 
   update() {}
