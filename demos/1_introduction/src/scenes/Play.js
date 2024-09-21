4ef3fefb7a110cb65d33909013def3b418b982ec
Nate Laffan <nate.laffan@gmail.com>
2024-09-21 12:18:00 -0700

    Finished introduction demo

--- demos/1_introduction/src/scenes/Play.js
+++ demos/1_introduction/src/scenes/Play.js
@@ -7,8 +7,10 @@ export class PlayScene extends Scene {
 
   create() {
     // Place the root group
-    console.log("PLAY!")
     const psd = this.P2P.place(this, "psd_key", "root");
+
+    // 🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸
+    // First, let's make all the blobs draggable.
 
     // Target the "blobs" group
     const blobs = psd.target("blobs");
@@ -22,6 +24,27 @@ export class PlayScene extends Scene {
         gameObject.x = dragX;
         gameObject.y = dragY;
       });
+    });
+
+    // 🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸
+    // Now, let's use the texture from a hidden layer to create an emitter.
+
+    // Get the x & y of our emit point.
+    const { x, y } = psd.target("emitPoint");
+    // Use the getTexture function to get the frames from the "seeds" spritesheet.
+    const emitTexture = this.P2P.getTexture(this, "psd_key", "root/seeds");
+
+    // Emit!
+    this.add.particles(x, y, emitTexture, {
+      frame: [0, 1, 2, 3, 5],
+      speedX: { min: -20, max: 20 },
+      speedY: { min: -10, max: 30 },
+      lifespan: { min: 3000, max: 5000 }, 
+      scale: { start: 1, end: 0.5 }, 
+      alpha: { start: 1, end: 0 }, 
+      angle: { min: -20, max: 20 }, 
+      gravityY: 100,
+      frequency: 100,
     });
   }
 
