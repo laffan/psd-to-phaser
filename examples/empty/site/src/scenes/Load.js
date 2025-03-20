bb906fb9a12c277b144f4a09ff2bc06dc67f8179
Nate <laffan@users.noreply.github.com>
2025-03-20 14:22:13 -0700

    Empty updates

--- examples/empty/site/src/scenes/Load.js
+++ examples/empty/site/src/scenes/Load.js
@@ -7,23 +7,10 @@ export class LoadScene extends Phaser.Scene {
     // Start loading PSD data
     this.P2P.load.load(this, "psd_key", "assets/empty");
 
-    // Listen for PSD asset loading progress
-    this.events.on("psdLoadProgress", (value) => {
-      // console.log("Loading progress:", value);
-    });
-
     // Listen for PSD asset loading completion
-    this.events.once(
-      "psdLoadComplete",
-      () => {
+    this.events.once( "psdLoadComplete", () => {
         this.scene.start("PlayScene");
-      },
-      this
-    );
-
-    // Add error listener
-    this.load.on("loaderror", (fileObj) => {
-      console.error("Error loading file:", fileObj.key);
-    });
+      }, this );
+      
   }
 }
